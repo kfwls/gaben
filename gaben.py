950d8bc88b2415abc4eed3ed3dfa42815a4efda6
Kaelan Fouwels <kfouwels@outlook.com>
2015-03-17 17:55:36 +0000

    endpoint is kill :'(

--- gaben.py
+++ gaben.py
@@ -4,5 +4,9 @@ c = 0;
 while True:
     c += 1
     r = requests.post("http://hunks.heatworld.com/appender.php", data=dat)
+    if r.status_code != 200:
+        print("Endpoint is kill, waiting 60 seconds")
+        time.sleep(60)
+
     print(str(c) + " : " + str(r.status_code))
 
